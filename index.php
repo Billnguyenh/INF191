d9ed8699976ff11b7fe7403fdb17f521e894be9e
Sam Applebaum <samappletree@gmail.com>
2017-03-05 23:04:00 -0800

    Added Confirm Password checking on Signup page

--- index.php
+++ index.php
@@ -59,19 +59,39 @@
      <div class="container">
          <form class="form-signin" role="form">
              <h2 class="form-signin-heading">Please sign in</h2>
-             <input type="email" class="form-control" placeholder="Email address" autofocus="">
-             <input type="password" class="form-control" placeholder="Password">
+             <div class="row">
+               <div class="form-group floating-label-form-group">
+                  <label>Email Address</label>
+                   <input id="emailTxt" type="email" class="form-control" placeholder="Email address" autofocus="">
+                </div>
+             </div>
+             <div class="row">
+             <div class="form-group floating-label-form-group">
+                <label>Password</label>
+                 <input type="password" class="form-control" placeholder="Password">
+              </div>
+           </div>
+             
              <label class="checkbox">
                  <input type="checkbox" value="remember-me">
                  Remember me
              </label>
-             <a class="btn btn-lg btn-primary btn-block" type="submit" href="notifications.html">Sign in</a>
+             <a class="btn btn-lg btn-primary btn-block" type="submit" href="notifications.php">Sign in</a>
              <a class="btn btn-lg btn-primary btn-block" href="signup.html" role="button">Get Started&nbsp;</a>
          </form>
      </div><!-- /container --><!-- Bootstrap core JavaScript
    ================================================== --><!-- Placed at the end of the document so the pages load faster --><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
    <script type="text/javascript">
+      $(function() {
+          $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+              $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+          }).on("focus", ".floating-label-form-group", function() {
+              $(this).addClass("floating-label-form-group-with-focus");
+          }).on("blur", ".floating-label-form-group", function() {
+              $(this).removeClass("floating-label-form-group-with-focus");
+          });
+      });
       if (window.localStorage) {
         var email = document.getElementById('emailTxt');
 
